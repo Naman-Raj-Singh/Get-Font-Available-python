b3de399c5bc6b42908ccb3bf04ba3a6d9489834a
Naman Raj Singh <76091786+Naman-Raj-Singh@users.noreply.github.com>
2020-12-18 12:57:11 +0530

    Create Fonts.py

---
+++ Fonts.py
@@ -0,0 +1,9 @@
+# you must install python. For help see this vedio, link is https://youtu.be/GwCZ2KElNdw
+from tkinter import *
+from tkinter import font
+import time
+root=Tk()
+fonts=font.families()
+for i in fonts:
+    print(i)
+time.sleep(100)
